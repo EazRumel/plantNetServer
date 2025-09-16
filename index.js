619462b7b14c4ffd1182d4c2ab809db2eb5b10a0
Eaz Rumel <eazrumel@Eazs-MacBook-Air.local>
2025-09-16 13:30:38 +0600

    updated the get api for reviews

--- index.js
+++ index.js
@@ -36,13 +36,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+
+     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    
+
+    const reviewCollection = client.db("treePlanet").collection("reviews")
+
+
+
+
+    
+  app.get("/reviews",async(req,res)=>{
+    const result = await reviewCollection.find().toArray();
+    res.send(result);
+  })
+
+
+
+
+
+
+
+await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+
+   
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
